c734bb324a864bf2681449ec438479a77838c502
juyifs <160045693+juyifs@users.noreply.github.com>
2026-04-08 13:11:47 +0800

    Update editor.rs

--- helix-view/src/editor.rs
+++ helix-view/src/editor.rs
@@ -8,7 +8,7 @@ use crate::{
     graphics::{CursorKind, Rect},
     handlers::Handlers,
     info::Info,
-    input::KeyEvent,
+    input::{KeyEvent, MouseClicks},
     register::Registers,
     theme::{self, Theme},
     tree::{self, Tree},
@@ -1259,6 +1259,7 @@ pub struct Editor {
 
     pub mouse_down_range: Option<Range>,
     pub cursor_cache: CursorCache,
+    pub mouse_clicks: MouseClicks,
 }
 
 pub type Motion = Box<dyn Fn(&mut Editor)>;
@@ -1381,6 +1382,7 @@ impl Editor {
             handlers,
             mouse_down_range: None,
             cursor_cache: CursorCache::default(),
+            mouse_clicks: MouseClicks::new(),
             dir_stack: VecDeque::with_capacity(DIR_STACK_CAP),
         }
     }
